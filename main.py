6cf1616433fd488babd15f786e281cb39cc3a0cd
Yaroslav K <69163097+Yaroslav-K-V@users.noreply.github.com>
2024-10-29 10:18:58 +0200

    main.py
    Was added the custom level with ability change the amount of chances and high number for random guessed number.
    Additionally, was added to the saving function ability to save the type of level, that player play. 
    Was remove the library OS, that doesn't used in code.

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import random
 import time
-import os
 import json
 
-name=None
 
-def save_results(name="unnamed",attemts=None,time_of_game=None):
+
+def save_results(name="unnamed", attemts=None, time_of_game=None, level=None):
     file = open("results.txt", "a")
-    results_of_user = json.dumps({"name":name,"attemts":attemts,"time_of_game":time_of_game})
+    results_of_user = json.dumps({"name" : name, "difficult" : level, "attemts":attemts, "time_of_game":time_of_game, "level":level})
     file.write('\n'+results_of_user)
     file.close()
 
 def show_leaderboard():
     with open("results.txt", "r") as file:
         for line in file:
-            if line.strip(): 
-                results_of_user = json.loads(line) 
+            if line.strip():
+                results_of_user = json.loads(line)
                 print("Player:", results_of_user['name'])
                 print("Attempts:", results_of_user['attemts'])
                 print("Time in game (seconds):", results_of_user['time_of_game'])
+                print("The level was:", results_of_user['level'])
                 print("-" * 30)
 
 
-def game(chances):
-    name=input("Please enter your name:\n")
-    start_time = time.time()
-    unknown_digit = random.randint(1, 100)
+def game(chances,level,custom=0,high_number=100):
+
+    if custom==1:
+        high_number=int(input("Please enter the high number for generation:\n"))
+        unknown_digit = random.randint(1, high_number)
+    elif custom==0:
+        unknown_digit = random.randint(1, 100)
     attempts = 0
+
+    name = input("Please enter your name:\n")
+    start_time = time.time()
 
     while attempts < chances:
         entered_digit = input("Enter the digit:\n")
@@ -48,7 +54,7 @@ def game(chances):
             time_of_game=end_time-start_time
             time_of_game=int(time_of_game)
             print(f"Congratulations! {name} guessed the correct number in {attempts} attempts and in {time_of_game} seconds.")
-            save_results(name,attempts,time_of_game)
+            save_results(name,attempts,time_of_game,level)
 
             return
 
@@ -66,8 +72,9 @@ def print_help_text():
           "\nEnter 2 for medium level"
           "\nEnter 3 for hard level"
           "\nEnter 4 for unlimited level"
-          "\nEnter 5 for help"
-          "\nEnter 6 for show a leaderboard"
+          "\nEnter 5 for custom level"
+          "\nEnter 7 for help"
+          "\nEnter 8 for show a leaderboard"
           "\nEnter 0 for exit ")
 
 while True:
@@ -76,19 +83,22 @@ while True:
         action = int(input("Enter your action:\n"))
         if action == 1:
             print("Great!You select a easy level")
-            game(chances=10)
+            game(chances=10,level='Easy level')
         elif action == 2:
             print("Great!You select a medium level")
-            game(chances=5)
+            game(chances=5,level='Medium level')
         elif action == 3:
             print("Great!You select a hard level")
-            game(chances=3)
+            game(chances=3,level='Hard level')
         elif action == 4:
             print("Great!You select a unlimited level")
-            game(chances=999999)
+            game(chances=999999,level='Unlimited level')
         elif action == 5:
+            print("Great!You select a custom level")
+            game(chances=int(input("Please enter the amount of chances:\n")), level='Custom level',custom=1)
+        elif action == 7:
             print_help_text()
-        elif action == 6:
+        elif action == 8:
             show_leaderboard()
         elif action == 0:
             exit()
